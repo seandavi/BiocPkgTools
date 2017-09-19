bba2415e291fdf5d9ee88f7acc9d91b406d0896e
lshep <shepherl@gmail.com>
2017-09-19 11:17:51 -0400

    Update getPackageInfo
      - use internal base R functions rather than outside contributed package

--- R/getPackageInfo.R
+++ R/getPackageInfo.R
@@ -1,32 +1,24 @@
 #' Generate needed inforamation to create DOI from a package
-#' DESCRIPTION file
+#' directory
 #'
-#' @param file character(1) Path to DESCRIPTION file
+#' @param dir character(1) Path to package
 #'
 #' @return a \code{data.frame}
 #'
-#' @import desc
-#'
 #' @export
-getPackageInfo <- function(file){
-
-    fnd = grep("Authors@R", readLines(file))
-    
-    if(length(fnd) == 0L){
-  # if Author present not Authors@R
-        desc = read.dcf(file, fields=c("Package", "Author"))
-        desc
-    }else{
- # if Authors@R present
-        require(desc)
-        desc =  description$new(file)
-        aut = desc$get_authors()
-        vl = lapply(aut, FUN=function(name){
+getPackageInfo <- function(dir){
+    db <- tools:::.read_description(file.path(dir, "DESCRIPTION"))
+    Package <- unname(db["Package"])
+    aar <- db["Authors@R"]
+    if (is.na(aar)) {
+        Author <- unname(db["Author"])
+    } else {
+        aut <- utils:::.read_authors_at_R_field(aar)
+        vl <- lapply(aut, FUN=function(name){
             tmp = strsplit(as.character(name), split="\\s+")[[1]]
             paste(tmp[1], tmp[2])
         })
-        Author = paste(unlist(vl), collapse=", ")
-        Package = unname(desc$get("Package"))
-        cbind(Package, Author)
+        Author <- paste(unlist(vl), collapse=", ")
     }
+    cbind(Package, Author)
 }
