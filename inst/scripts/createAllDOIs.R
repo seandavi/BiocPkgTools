368b0c97ef4a6d712de86742ec750b3d4d2cdcbe
LiNk-NY <mramos09@gmail.com>
2019-04-23 17:53:03 -0400

    use biocPkgList in script

--- inst/scripts/createAllDOIs.R
+++ inst/scripts/createAllDOIs.R
@@ -1,4 +1,10 @@
 library(BiocPkgTools)
-pl = getBiocPkgList()
+pl = biocPkgList()
 testing = TRUE # change to FALSE to actually update/create records
-z = mapply(FUN = generateBiocPkgDOI,pl$Package,pl$Author,2017,testing=testing)
+z = mapply(
+    FUN = BiocPkgTools:::generateBiocPkgDOI,
+    pl$Package,
+    pl$Author,
+    2017,
+    testing=testing
+)
