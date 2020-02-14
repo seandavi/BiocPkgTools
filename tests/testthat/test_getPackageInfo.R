b7c9ef02afbb76dc166a20fcf47530768256de0c
FelixErnst <felix.gm.ernst@outlook.com>
2020-02-14 20:21:29 +0100

    fixed regression for input check of getPackageInfo()

---
+++ tests/testthat/test_getPackageInfo.R
@@ -0,0 +1,13 @@
+context("githubURLParts")
+library(BiocPkgTools)
+
+test_that("getPackageInfo", {
+    expect_error(getPackageInfo("../test"),
+                 'dir "../test" does not exist')
+    expect_error(getPackageInfo("../"),
+                 'dir "../" does not contain')
+    actual <- getPackageInfo(system.file(package = "BiocManager"))
+    expect_true(is.matrix(actual))
+    expect_type(actual,"character")
+    expect_equal(colnames(actual),c("Package","Author"))
+})
