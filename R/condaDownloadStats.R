280e4150add5b74fa356b459d5f6e0d0d8e9e7d9
Mike Smith <grimbough@gmail.com>
2021-02-24 16:43:42 +0100

    Add manpage for anacondaDownloadStats

--- R/condaDownloadStats.R
+++ R/condaDownloadStats.R
@@ -23,16 +23,16 @@
 #' @export
 anacondaDownloadStats = function() {
   temp_file = tempfile()
-  url = 'https://github.com/grimbough/anaconda-download-stats/blob/master/rdata/bioc_counts.rds?raw=true'
+  url = 'https://github.com/grimbough/anaconda-download-stats/raw/master/rdata/bioc_counts.rds'
   download.file(url, destfile = temp_file, quiet = TRUE)
   
   tmp = readRDS(temp_file)
   tmp$repo = 'Anaconda'
   tmp$Nb_of_distinct_IPs = NA_integer_
-
+  
   tmp = as_tibble(tmp) %>%
-    dplyr::mutate(Date = as.Date(paste(.data$Year, .data$Month, '01'),
-                                 '%Y %b %d')) %>%
+    mutate(Date = as.Date(paste(.data$Year, .data$Month, '01'),
+                          '%Y %b %d')) %>%
     select('Package', 'Year', 'Month', 'Nb_of_distinct_IPs', 'Nb_of_downloads', 'repo', 'Date')
   
   class(tmp) = c('bioc_downloads', class(tmp))
