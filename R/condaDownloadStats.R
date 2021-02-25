80fd7f3beab32a545ae6feb81faf91bb467bcc4d
Mike Smith <grimbough@gmail.com>
2021-02-25 17:33:05 +0100

    Sort anaconda count tables to match Bioc

--- R/condaDownloadStats.R
+++ R/condaDownloadStats.R
@@ -1,13 +1,15 @@
+utils::globalVariables(c("Package", "Year", "Date"))
+
 #' Get download statistics for Bioconductor packages distributed via
 #' Anaconda.
 #'
 #' @details Anaconda provide daily download counts for all software packages
-#' they distribute.  The are summarised into monthly tables of counts and made 
+#' they distribute.  These are summarised into monthly tables of counts and made 
 #' available from https://github.com/grimbough/anaconda-download-stats
 #' This function provides a mechanism to download these monthly counts for 
-#' Bioconductor packages distributed through Anaconda.#' 
+#' Bioconductor packages distributed through Anaconda. 
 #'
-#' @importFrom dplyr mutate select %>%
+#' @importFrom dplyr mutate select arrange desc %>%
 #' @importFrom utils read.table download.file
 #' @importFrom tibble as_tibble
 #'
@@ -15,7 +17,10 @@
 #' all Bioconductor packages distributed by Anaconda, in tidy format.
 #' Note: Anaconda do not provide counts for unique IP addresses.  This column
 #' is listed as \code{NA} for all packages to provide continuity with data from
-#' Bioconductor.org obtained by \code{\link{biocDownloadStats()}}
+#' Bioconductor.org obtained by \code{\link{biocDownloadStats}}.  The counts are
+#' updated monthly, so do not expect to see counts for the current month.
+#' 
+#' @author Mike L. Smith
 #'
 #' @examples
 #' anacondaDownloadStats()
@@ -33,7 +38,9 @@ anacondaDownloadStats = function() {
   tmp = as_tibble(tmp) %>%
     mutate(Date = as.Date(paste(.data$Year, .data$Month, '01'),
                           '%Y %b %d')) %>%
-    select('Package', 'Year', 'Month', 'Nb_of_distinct_IPs', 'Nb_of_downloads', 'repo', 'Date')
+    select('Package', 'Year', 'Month', 'Nb_of_distinct_IPs', 'Nb_of_downloads', 'repo', 'Date') %>%
+    ## put this into the same order as the BioC table
+    arrange(tmp, Package, desc(Year), Date)
   
   class(tmp) = c('bioc_downloads', class(tmp))
   tmp
