d697871fbee2c5c425f7204eb382272d6981c99f
LiNk-NY <mramos09@gmail.com>
2021-07-29 17:51:46 -0400

    add biocMaintained function

---
+++ R/biocMaintained.R
@@ -0,0 +1,33 @@
+#' Bioconductor Maintained Packages
+#'
+#' List all the packages associated with a maintainer. By default, it will
+#' return all packages associated with the `maintainer@bioconductor.org` email.
+#'
+#' @inheritParams biocBuildReport
+#'
+#' @param main character(1) The regex for searching through the Maintainer
+#' column as obtained from `biocPkgList()`.
+#'
+#' @md
+#'
+#' @examples
+#'
+#' biocMaintained()
+#'
+#' @export
+biocMaintained <-
+    function(
+        version = BiocManager::version(),
+        main = "maintainer@bioconductor\\.org"
+    )
+{
+    pkgs <- BiocPkgTools::biocPkgList()
+    biocmaint <- vapply(
+        pkgs$Maintainer,
+        function(x)
+            any(grepl("maintainer@bioconductor\\.org", x)),
+        logical(1L)
+    )
+    pkgs[biocmaint, ]
+}
+
