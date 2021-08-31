8521afeddcf7c96217934334e424df951dc265ed
LiNk-NY <mramos09@gmail.com>
2021-08-31 13:35:13 -0400

    update remaining tests

--- tests/testthat/test_biocBuildReport.R
+++ tests/testthat/test_biocBuildReport.R
@@ -9,25 +9,16 @@ test_that("columns are consistent in report output", {
     bioc3.12_build <- biocBuildReport("3.12")
     expect_true(all(columnames %in% colnames(bioc3.12_build)))
     expect_true(inherits(bioc3.12_build[["git_last_commit_date"]], "POSIXct"))
+    expect_true(tibble::is_tibble(bioc3.12_build))
 
     bioc3.13_build <- biocBuildReport("3.13")
     expect_true(all(columnames %in% colnames(bioc3.13_build)))
     expect_true(inherits(bioc3.13_build[["git_last_commit_date"]], "POSIXct"))
+    expect_true(tibble::is_tibble(bioc3.13_build))
 
     biocdevel_build <- biocBuildReport("3.14")
     expect_true(all(columnames %in% colnames(biocdevel_build)))
     expect_true(inherits(biocdevel_build[["git_last_commit_date"]], "POSIXct"))
+    expect_true(tibble::is_tibble(biocdevel_build))
 })
 
-test_that("biocBuildReport returns appropriate class", {
-    expect_true(tibble::is_tibble(bioc3.12_build))
-})
-
-test_that("nrow is correct", {
-  # This test fails when the build system changes
-  # expect_equal(nrow(bioc3.12_build), 27466)
-})
-
-test_that("ncol is correct", {
-  expect_equal(ncol(bioc3.12_build), 9)
-})
