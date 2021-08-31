cf6a3476eeedd2d6cb70b9b0440e65dbee5564d5
LiNk-NY <mramos09@gmail.com>
2021-08-31 12:42:50 -0400

    update biocBuildReport output consistency; add tests

--- tests/testthat/test_biocBuildReport.R
+++ tests/testthat/test_biocBuildReport.R
@@ -2,9 +2,22 @@ context("biocBuildReport")
 library(BiocPkgTools)
 
 # test release build report
-bioc3.12_build <- biocBuildReport("3.12")
-bioc3.13_build <- biocBuildReport("3.13")
-biocdevel_build <- biocBuildReport("3.14")
+test_that("columns are consistent in report output", {
+    columnames <- c("pkg", "author", "version", "git_last_commit",
+        "git_last_commit_date", "node", "stage")
+
+    bioc3.12_build <- biocBuildReport("3.12")
+    expect_true(all(columnames %in% colnames(bioc3.12_build)))
+    expect_true(inherits(bioc3.12_build[["git_last_commit_date"]], "POSIXct"))
+
+    bioc3.13_build <- biocBuildReport("3.13")
+    expect_true(all(columnames %in% colnames(bioc3.13_build)))
+    expect_true(inherits(bioc3.13_build[["git_last_commit_date"]], "POSIXct"))
+
+    biocdevel_build <- biocBuildReport("3.14")
+    expect_true(all(columnames %in% colnames(biocdevel_build)))
+    expect_true(inherits(biocdevel_build[["git_last_commit_date"]], "POSIXct"))
+})
 
 test_that("biocBuildReport returns appropriate class", {
     expect_true(tibble::is_tibble(bioc3.12_build))
