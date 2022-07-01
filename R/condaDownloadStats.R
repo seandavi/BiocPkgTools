767433a9a3a81724d3b424ae2411907a3880ee74
LiNk-NY <marcel.ramosperez@roswellpark.org>
2022-06-30 20:51:04 -0400

    minor whitespace changes

--- R/condaDownloadStats.R
+++ R/condaDownloadStats.R
@@ -4,10 +4,10 @@ utils::globalVariables(c("Package", "Year", "Date"))
 #' Anaconda.
 #'
 #' @details Anaconda provide daily download counts for all software packages
-#' they distribute.  These are summarised into monthly tables of counts and made 
+#' they distribute.  These are summarised into monthly tables of counts and made
 #' available from https://github.com/grimbough/anaconda-download-stats
-#' This function provides a mechanism to download these monthly counts for 
-#' Bioconductor packages distributed through Anaconda. 
+#' This function provides a mechanism to download these monthly counts for
+#' Bioconductor packages distributed through Anaconda.
 #'
 #' @importFrom dplyr mutate select arrange desc %>%
 #' @importFrom utils read.table download.file
@@ -19,7 +19,7 @@ utils::globalVariables(c("Package", "Year", "Date"))
 #' is listed as \code{NA} for all packages to provide continuity with data from
 #' Bioconductor.org obtained by \code{\link{biocDownloadStats}}.  The counts are
 #' updated monthly, so do not expect to see counts for the current month.
-#' 
+#'
 #' @author Mike L. Smith
 #'
 #' @examples
@@ -30,18 +30,18 @@ anacondaDownloadStats = function() {
   temp_file = tempfile()
   url = 'https://github.com/grimbough/anaconda-download-stats/raw/master/rdata/bioc_counts.rds'
   download.file(url, destfile = temp_file, quiet = TRUE)
-  
+
   tmp = readRDS(temp_file)
   tmp$repo = 'Anaconda'
   tmp$Nb_of_distinct_IPs = NA_integer_
-  
+
   tmp = as_tibble(tmp) %>%
     dplyr::mutate(Date = as.Date(paste(.data$Year, .data$Month, '01'),
                           '%Y %b %d')) %>%
     select('Package', 'Year', 'Month', 'Nb_of_distinct_IPs', 'Nb_of_downloads', 'repo', 'Date') %>%
     ## put this into the same order as the BioC table
     arrange(tmp, Package, desc(Year), Date)
-  
+
   class(tmp) = c('bioc_downloads', class(tmp))
   tmp
 }
