6b8059c5c1dfc0544971068e4aeb706fe38be2a7
LiNK-NY <mramos09@gmail.com>
2022-11-09 16:25:44 -0500

    update biocConfigVersion function to configure dates and snapshots env / options pertinent to BiocManager

--- R/biocConfigVersion.R
+++ R/biocConfigVersion.R
@@ -1,22 +1,110 @@
+.BIOC_CONFIG_YAML <- "https://bioconductor.org/config.yaml"
+
+#' Create a Bioconductor configuration that allows reproducibility
+#'
+#' This function helps users who wish to use a previously released Bioconductor
+#' version.
+#'
+#' @details The `version` argument is largely dictated by the current R /
+#'   Bioconductor installation, e.g., Bioconductor version 3.15. With this
+#'   information and the value of `getOption("BiocManager.snapshot")`, the
+#'   function will download a copy of the `config.yaml` file and modify it with
+#'   the appropriate date entry as returned from `biocLastBuildDate` for the
+#'   Bioconductor version in question. The function will also update the
+#'   `.Renviron` file with a `BIOCONDUCTOR_CONFIG_FILE` entry. See
+#'   `?BiocManager::install` for more details. This step can be skipped by
+#'   setting the `Renviron` argument to `NULL` if the `BIOCONDUCTOR_CONFIG_FILE`
+#'   option is set.
+#'
+#' @param version character(1) The desired version to reproduce. This is largely
+#'   dictated by the current R / Bioconductor version installed.
+#'
+#' @param lastBuildDate named character(1) Override argument to specify the date
+#'   of the desired CRAN snapshot, this date should agree or come close to the
+#'   date provided by `biocLastBuildDate`. The name of the character vector
+#'   should correspond to the Bioconductor version, e.g.,
+#'   `c('3.15'="2022-10-19")`. When `lastBuildDate` is not given, the `version`
+#'   argument is used in conjunction with the aforementioned function.
+#'
+#' @param snapshot character(1) The snapshot CRAN repository to use for
+#'   reproducibility. This defaults to the value of
+#'   `getOption("BiocManager.snapshot", "RSPM")`.
+#'
+#' @param yamlDir character(1) The folder location for the modified
+#'   `config.yaml` file. By default, it is set to the value of
+#'   `tools::R_user_dir("BiocPkgTools", which = "config")`.
+#'
+#' @param overwrite logical(1) Whether to overwrite the local `config.yaml`
+#'   file. Set to `FALSE` by default. Set to `TRUE` in order to save the
+#'   changes made to the configuration map.
+#'
+#' @param Renviron character(1) The file location of the `.Renviron` file to
+#'   update with the `BIOCONDUCTOR_CONFIG_FILE` variable. Set to `NULL` to avoid
+#'   writing in the `.Renviron`. The default directory location of the file is
+#'   given by `Sys.getenv("HOME")`.
+#'
+#' @return Mostly called for the side-effects of copying and modifying the
+#'   `config.yaml` and `.Renviron` files to reproduce an R / Bioconductor
+#'   package environment from a previous Bioconductor release.
+#'
+#' @examples
+#' biocConfigVersion("3.15", "RSPM")
+#' @export
 biocConfigVersion <- function(
-    version = "3.15", local = TRUE,
-    yaml_dir = tools::R_user_dir("BiocPkgTools", which = "config")
+    version = "3.15",
+    lastBuildDate = NULL,
+    snapshot = getOption("BiocManager.snapshot", "RSPM"),
+    yamlDir = tools::R_user_dir("BiocPkgTools", which = "config"),
+    overwrite = FALSE,
+    Renviron = file.path(Sys.getenv("HOME"), ".Renviron")
 ) {
-    if (!local)
-        stop("Currently only local 'config.yaml' files are supported")
-    date <- biocLastBuildDate(version)
-    yaml_file <- file.path(yaml_dir, "config.yaml")
-    download.file("https://bioconductor.org/config.yaml", yaml_file)
+    if (!requireNamespace("yaml", quietly = TRUE))
+        stop("Install the 'yaml' package to use 'biocConfigVersion'")
+    if (is.null(lastBuildDate))
+        lastBuildDate <- biocLastBuildDate(version = version)
+    if (!dir.exists(yamlDir))
+        dir.create(yamlDir)
+    ## download and edit config.yaml file
+    yaml_file <- file.path(yamlDir, "config.yaml")
+    if (!file.exists(yaml_file) || overwrite)
+        download.file(.BIOC_CONFIG_YAML, yaml_file)
+    config <- yaml::read_yaml(yaml_file)
+    if (!snapshot %in% c("RSPM", "MRAN"))
+        stop("Unknown 'snapshot' option; use either 'RSPM' or 'MRAN'")
+    mapname <- paste0(tolower(snapshot), "_ver_for_bioc_ver")
+    mapelement <- structure(list(as.list(lastBuildDate)), .Names = mapname)
+    indx <- which(names(config) == "release_dates") - 1L
+    config <- append(config, mapelement, after = indx)
+    if (overwrite) {
+        message("Updating 'config.yaml' at ", dirname(yaml_file))
+        yaml::write_yaml(config, file = yaml_file)
+    }
+
+    ## update Renviron with config.yaml location
+    if (!is.null(Renviron)) {
+        message("Updating ", Renviron, " ...")
+        .update_renviron(file = Renviron, yaml_file = yaml_file)
+    } else {
+        message(
+            "Set the 'BIOCONDUCTOR_CONFIG_FILE' envvar or option to:\n  ",
+            yaml_file,
+            "\nSee ?BiocManager::install for details"
+        )
+    }
 }
 
-## download.file("https://bioconductor.org/config.yaml", "~/config.yaml")
-## file.edit("~/config.yaml")
-##
-## ## add this to the yaml file
-## rspm_ver_for_bioc_ver:
-##   "3.15": "2022-10-19"
-##
-## ## set environment variable
-## file.edit("~/.Renviron")
-## #  BIOCONDUCTOR_CONFIG_FILE=/mnt/STORE1/bighome/mramos/config.yaml
-## # opt <- getOption("BiocManager.snapshot", "RSPM")
+.update_renviron <- function(file, yaml_file) {
+    if (!file.exists(file))
+        file.create(file)
+    lines <- readLines(file)
+    hasconfig <- any(grepl("BIOCONDUCTOR_CONFIG_FILE", lines))
+    if (hasconfig)
+        stop(
+            "'BIOCONDUCTOR_CONFIG_FILE' already exists; ",
+            "see ?BiocManager::install"
+        )
+    lines <- c(lines, paste0(
+        "BIOCONDUCTOR_CONFIG_FILE=",normalizePath(yaml_file, winslash = "/")
+    ))
+    writeLines(lines, file)
+}
